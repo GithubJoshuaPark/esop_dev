7313a61b01e3067bfaaf2fe8d311a2632fb1f943
parkjinwoo <soromiso@gmail.com>
2024-08-27 01:08:46 +0900

    feat: Page findScoreByStudent

--- src/main/java/com/soro/esop/repository/customizedRepository/SubjectScoreRepositoryCustom.java
+++ src/main/java/com/soro/esop/repository/customizedRepository/SubjectScoreRepositoryCustom.java
@@ -1,6 +1,8 @@
 package com.soro.esop.repository.customizedRepository;
 
 import com.soro.esop.model.SubjectScoreDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -18,4 +20,5 @@ import java.util.List;
 
 public interface SubjectScoreRepositoryCustom {
     List<SubjectScoreDto> findScoreByStudent(Long id);
+    Page<SubjectScoreDto> findScoreByStudent(Long id, Pageable pageable);
 }
--- src/main/java/com/soro/esop/repository/customizedRepository/SubjectScoreRepositoryCustomImpl.java
+++ src/main/java/com/soro/esop/repository/customizedRepository/SubjectScoreRepositoryCustomImpl.java
@@ -7,6 +7,9 @@ import com.soro.esop.entity.QScore;
 import com.soro.esop.entity.QStudent;
 import com.soro.esop.model.SubjectScoreDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 
 import java.util.List;
 
@@ -69,4 +72,46 @@ public class SubjectScoreRepositoryCustomImpl implements SubjectScoreRepositoryC
                         .build())
                 .toList();
     }
+
+    @Override
+    public Page<SubjectScoreDto> findScoreByStudent(Long id, Pageable pageable) {
+        QStudent st = QStudent.student;
+        QScore   sc = QScore.score1;
+
+        JPAQuery<Tuple> query = queryFactory
+                .select(
+                        st.studentId
+                        ,st.name
+                        ,st.age
+                        ,sc.score
+                        ,sc.subject
+                )
+                .from(st)
+                .join(sc)
+                .on(st.studentId.eq(sc.studentId))
+                .where(st.studentId.eq(id))
+                .orderBy(st.studentId.asc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize());
+
+        List<SubjectScoreDto> subjectScoreDtos = query.fetchJoin().fetch().stream()
+                .map(tuple -> SubjectScoreDto.builder()
+                        .studentId(tuple.get(st.studentId))
+                        .name(tuple.get(st.name))
+                        .age(tuple.get(st.age))
+                        .score(tuple.get(sc.score))
+                        .subject(tuple.get(sc.subject))
+                        .build())
+                .toList();
+
+        JPAQuery<Long> countQuery = queryFactory
+                .select(st.studentId)
+                .from(st)
+                .join(sc)
+                .on(st.studentId.eq(sc.studentId))
+                .where(st.studentId.eq(id));
+
+        return PageableExecutionUtils.getPage(subjectScoreDtos, pageable, countQuery::fetchCount);
+    }
+
 }
